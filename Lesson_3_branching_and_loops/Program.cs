19b7320b0d23741bc453f72faeebb84f26badff2
Viktorija <wiktoria.gr1@gmail.com>
2023-10-11 22:22:54 +0300

    Add project files.

---
+++ Lesson_3_branching_and_loops/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3_branching_and_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a game that can be played by two players.
+            // At the start, players are asked to enter their nicknames.
+            // The nicknames are stored until the end of the game.
+            // The program selects a random number, "gameNumber," between 12 and 50, informing the players of this number.
+            // Players take turns (the game informs about the current player's turn).
+            // The player whose turn it is enters a number, "userTry," which can be 1, 2, 3, or 4.
+            // The entered number is subtracted from "gameNumber."
+            // The new value of "gameNumber" is displayed to the players on the screen.
+            // The player who, after their turn, makes "gameNumber" equal to zero wins.
+            // The game congratulates the winner and offers a rematch.
+            // Demonstration
+            // Number: 12
+            // User1's Turn: 3
+            //
+            // Number: 9
+            // User2's Turn: 4
+            //
+            // Number: 5
+            // User1's Turn: 2
+            //
+            // Number: 3
+            // User2's Turn: 3
+            //
+            // User2 Wins!
+
+            string user1, user2;
+            int choice;
+            int gameNumber;
+            Random randomize= new Random();
+            Console.WriteLine("User 1, provide your nickname:");
+            user1=Console.ReadLine();
+            Console.WriteLine("User 1 name is " + user1+"\n User 2, provide your nickname: ");
+            user2 =Console.ReadLine();
+            Console.WriteLine("User 2 name is " + user2);
+            gameNumber = randomize.Next(12,51);
+            Console.WriteLine($"\nRandom number is {gameNumber}");
+            
+            for(int i = 0; gameNumber > 0; i++)
+            {
+                if(i%2 == 0)
+                {
+                    Console.WriteLine($"{user1}:");
+                    choice = Convert.ToInt32(Console.ReadLine());
+                    if (choice > 0 && choice < 5)
+                    {
+                        gameNumber = gameNumber - choice;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Number should be 1, 2, 3 or 4, you skipping you'r turn");
+                    }
+                    if (gameNumber == 0)
+                    {
+                        Console.WriteLine($"GameNumber is {gameNumber}, congratulations to {user1}");
+                    }
+                    else if (gameNumber < 0)
+                    {
+                        Console.WriteLine($"Gamenumber is {gameNumber}, {user1} loosed");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"GameNumber now is {gameNumber}");
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine($"{user2}:");
+                    choice = Convert.ToInt32(Console.ReadLine());
+                    if (choice > 0 && choice < 5)
+                    {
+                        gameNumber = gameNumber - choice;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Number should be 1, 2, 3 or 4, you skipping you'r turn");
+                    }
+                    if (gameNumber == 0)
+                    {
+                        Console.WriteLine($"GameNumber is {gameNumber}, congratulations to {user2}");
+                    }
+                    else if (gameNumber < 0)
+                    {
+                        Console.WriteLine($"Gamenumber is {gameNumber}, {user2} loosed");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"GameNumber now is {gameNumber}");
+                    }
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Lesson_3_branching_and_loops/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lesson_3_branching_and_loops")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Lesson_3_branching_and_loops")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9633e2c6-d352-4490-95eb-6aa7c662d776")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
